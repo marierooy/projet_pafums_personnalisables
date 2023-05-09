a2d2c00883e73d2aa0a83a6dfdc44e6feb915660
Marie Rooy <rooy.marie@gmail.com>
2023-05-09 15:54:46 +0200

    version 9 mai 3

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -140,6 +140,7 @@ class CartController extends AbstractController
                 $text = '<h1>Bonjour</h1>
                 <p>Voici les détails de votre commande</p>
                 <ul>';
+                var_dump($text);
 
                 foreach($newProduct->getProducts() as $key => $product) {
                     $purchased = new PurchasedProduct;
@@ -152,7 +153,6 @@ class CartController extends AbstractController
                     $text.='<li>'.$product.getName().' <br> Prix: '.$purchased.getQuantity().'€ x '.$purchased.getUnitPrice().'</li>';
                 }
                 $text.='</ul><br><p>Total: '.$order.getTotal().'€</p>';
-                echo $text;
                 $email = (new Email())
                 ->from('identite-olfactive@ecom.fr')
                 ->to($user->getEmail())
