3d880f6248a2ae6cfdd7d9b48c9af8b5fde43f57
Marie Rooy <rooy.marie@gmail.com>
2023-05-22 17:06:27 +0200

    version 22 mai 27

--- src/Form/CreatedPerfumeType.php
+++ src/Form/CreatedPerfumeType.php
@@ -22,7 +22,7 @@ class CreatedPerfumeType extends AbstractType
                 'attr' => [
                   'min' => 0,
                   'max' => 100,
-                  'maxlength' => 3,
+                  'placeholder' => 'number - 3 digits',
                   'int' => true,
                 ]
             ])
@@ -31,7 +31,7 @@ class CreatedPerfumeType extends AbstractType
                 'attr' => [
                   'min' => 0,
                   'max' => 100,
-                  'maxlength' => 3,
+                  'placeholder' => 'number - 3 digits',
                   'int' => true,
                 ]
             ])
@@ -40,7 +40,7 @@ class CreatedPerfumeType extends AbstractType
                 'attr' => [
                   'min' => 0,
                   'max' => 100,
-                  'maxlength' => 3,
+                  'placeholder' => 'number - 3 digits',
                   'int' => true,
                 ]
             ])
